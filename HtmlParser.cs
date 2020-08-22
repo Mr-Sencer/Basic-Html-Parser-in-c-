a1b1515b8622a993366348bea55eae6d4b3a1292
Mr-Sencer <husencerdiba@gmail.com>
2020-08-22 23:16:20 +0300

    Update HtmlParser.cs

--- HtmlParser.cs
+++ HtmlParser.cs
@@ -6,61 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebProject.Html
 {
-    public class LexerParser
-    {
-        int index = 0;
-    }
-    public class MathParser
-    {
-        int left = 0;
-        // 3+2+6 
-        // 
-        void go(int index,string value,ref int loc,ref int number)
-        {
-            StringBuilder b = new StringBuilder();
-             for (int i=index+1;i<value.Length;i++)
-            {
-              if(value[i]=='+' || value[i]=='-' ||value[i]=='*' ||value[i]=='/' ||  i==value.Length-1)
-                {
-                    loc = i;
-                    number = Convert.ToInt32( b.ToString());
-                    break;
-                }
-                else
-                {
-                    b.Append(value[i]);
-                }
-            }
-        }
-        int right = 0;
-    public    int number = 0;
-        public void Match(string value)
-        {
-            parse(value);
-        }
-        void parse(string value)
-        {
-            StringBuilder b = new StringBuilder();
-            int l = 0;
-            for (int i=0;i<value.Length;i++)
-            {
-                if(value[i]=='+')
-                {
-                    left = Convert.ToInt32(b.ToString());
-
-                    go(i, value, ref l, ref left);
-                    number += left - right;
-                    i = l;
-                    continue; 
-
-                }
-                if (char.IsWhiteSpace(value[i])==false && (value[i]!='+' && value[i]!= '-' && value[i] != '*') && value[i] != '/')
-                {
-                    b.Append(value[i]);
-                }
-            }
-        }
-    }
+   
 
     public   class HtmlParser
     {
@@ -74,7 +20,7 @@ namespace WebProject.Html
             }
             TextProcess process = new TextProcess();
             HtmlTag element; 
-        //(2+5+5)*5 +3
+    
             for (int i = 0; i < html.Length; i++) 
             {
                 
